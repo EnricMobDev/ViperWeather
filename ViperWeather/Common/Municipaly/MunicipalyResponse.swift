994949354350117283c6e39ab58e860a6b023669
Enric pou Villanueva <epouvillanueva@gmail.com>
2018-10-04 17:17:14 +0200

    ADD: Create the municipality response

---
+++ ViperWeather/Common/Municipaly/MunicipalyResponse.swift
@@ -0,0 +1,9 @@
+//
+//  MunicipalyResponse.swift
+//  ViperWeather
+//
+//  Created by Enric Pou Villanueva on 04/10/2018.
+//  Copyright © 2018 Enric Pou Villanueva. All rights reserved.
+//
+
+import Foundation

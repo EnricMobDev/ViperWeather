6485febd511534e9879bc23de0089c1e4d8ffa6e
Enric Pou Villanueva <enric.pou.villanueva@everis.com>
2018-10-09 11:56:39 +0200

    ADD structure to create drawer protocols

---
+++ ViperWeather/Modules/Home/Cells/HomeDetailListCell.swift
@@ -0,0 +1,18 @@
+//
+//  HomeDetailListCell.swift
+//  ViperWeather
+//
+//  Created by Enric Pou Villanueva on 09/10/2018.
+//  Copyright © 2018 Enric Pou Villanueva. All rights reserved.
+//
+
+import UIKit
+
+class HomeDetailListCell: UITableViewCell {
+    
+    @IBOutlet weak var hourLabel: UILabel!
+    @IBOutlet weak var setHourLabel: UILabel!
+    @IBOutlet weak var stateLabel: UILabel!
+    @IBOutlet weak var setStateLabel: UILabel!
+    
+}

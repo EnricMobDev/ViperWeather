58b7899242be2e7a014d67d455cc682b7613f104
Dmytro Pylypenko <dimpiax@gmail.com>
2018-11-10 12:58:59 +0100

    Add UI Test example

--- ViperWeatherUITests/ViperWeatherUITests.swift
+++ ViperWeatherUITests/ViperWeatherUITests.swift
@@ -29,6 +29,11 @@ class ViperWeatherUITests: XCTestCase {
     func testExample() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        
+        let app = XCUIApplication()
+        app.buttons["Sign In"].tap()
+        app.tables/*@START_MENU_TOKEN@*/.cells.containing(.staticText, identifier:"07:00h")/*[[".cells.containing(.staticText, identifier:\"Muy nuboso\")",".cells.containing(.staticText, identifier:\"07:00h\")"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.buttons["More Details"].tap()
     }
 
 }
